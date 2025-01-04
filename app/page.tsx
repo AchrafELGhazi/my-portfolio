26c03313946df4b7e114c09907e32818a30961ca
AchrafElGhazi <a.elghazi@aui.ma>
2025-01-04 03:12:07 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
+import TechStack from "@/components/TechStack";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
@@ -19,6 +20,7 @@ const Home = () => {
         <Hero />
         <Grid />
         <RecentProjects />
+        <TechStack />
         <Clients />
         <Experience />
         <Approach />

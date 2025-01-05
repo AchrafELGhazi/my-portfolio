5072b8b8c16d6f157eec462ddccff5ad1b9f521e
AchrafElGhazi <a.elghazi@aui.ma>
2025-01-05 02:04:37 +0100

    commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  // analyzerMode: 'static',
+  // analyzerPort: 8888,
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
